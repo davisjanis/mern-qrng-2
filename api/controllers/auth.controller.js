352b377b6b80faa32102983cef9fe11fb8691c65
davisjanis <davislarins@gmail.com>
2023-12-27 14:46:04 +0100

    added middleware and functions to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,20 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     //destructure request body from POST method
     const {username, email, password} = req.body;
     // get password and hashit
     const hashedPassword = bcryptjs.hashSync(password, 10);
+    
     const newUser = new User({username, email, password: hashedPassword});
     try {
         await newUser.save()
         res.status(201).json({message: "User Created successfully"});
     
     } catch (error) {
-        res.status(500).json(error.message);
+        next(errorHandler(300, "smth went wrong"));
     }
 
     
--- api/index.js
+++ api/index.js
@@ -13,14 +13,32 @@ mongoose.connect(process.env.MONGO).then(() => {
         console.log(err);
     });
 
+    //start the app
 const app = express();
 
 //to receive json as input
 app.use(express.json());
 
+//app use the port
 app.listen(3000, () => {
     console.log('Server listening on port 3000!');
 }); 
 
+//MOUNT MIDDLEWARE FUNCTIONS (app.use is Express method to do that)
+// middleware funcs are functions that have avvess to the req or res object, and the next middleware func in the apps request-response cycle.
+
+
 app.use('/api/user', userRoutes);
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    })
+}
+    )
